326b3bb045104948fb140ae7745e58c49e92aa7c
hzwei2017 <649148144@qq.com>
2024-06-18 09:12:26 +0800

    探索模式的新挂季票方案

--- Fame_Pass.py
+++ Fame_Pass.py
@@ -1,26 +1,27 @@
 # ======README======
-# 适用每周专题：    重力蜂窝迷环试炼
+# 适用每周专题：    探索
 # 适用屏幕比例：    全比例
 # 适用分辨率：      全分辨率（原始坐标依据1920*1080分辨率编写）
-# 最后编辑日期：    2024年04月18日
-# 最后版本：        v6.0
+# 最后编辑日期：    2024年06月16日
+# 最后版本：        v6.1
 # 
 # 2023-11-26 | v3.1 | 1、关闭func3；将进入游戏检测和卡关检测合并，计数移给func1来判定
 # 2023-11-27 | v3.2 | 2、将卡关检测时间从70s改成150s；删除以前的是否进入游戏检测
-# 2023-11-30 | v4.0 | 4、将跳水和进入游戏检测合并
-# 2023-12-02 | v4.1 | 5、把退出和回车合并；在大厅准备之前加入是否满级判定，并于满级后自动发送邮件通知
-# 2023-12-03 | v4.2 | 6、启用git管理
-# 2023-12-07 | v5.0 | 7、关闭git管理；将代码适配全分辨率、全比例屏幕
-# 2023-12-15 | v5.1 | 8、多进程模式改为多线程模式以降低资源占用
-# 2023-12-22 | v5.2 | 9、由于糖豆人将每周专题获得的经验降为20xp，所以该脚本失去意义了
-# 2024-01-30 | v5.3 | 10、糖豆人恢复每周专题经验值
-# 2024-01-31 | v5.4 | 11、加入季票等级平均执行效率变量，并监控该变量，在异常时发送邮件提醒
-# 2024-02-22 | v5.5 | 12、修复监控脚本执行效率的bug；双倍经验日开始（2024-03-05 20:00结束）
-# 2024-02-22 | v5.6 | 13、删除邮件装饰器和func4
-# 2024-03-05 | v5.7 | 14、暂停func3；适配生存休闲模式；双倍经验日结束
-# 2024-03-13 | v5.8 | 15、适配混合生存赛；更改func1中经验值计算方式
-# 2024-03-22 | v5.9 | 16、适配重力蜂窝迷环试炼
-# 2024-04-18 | v6.0 | 17、双倍经验日开始（2024-04-30 20:00结束）
+# 2023-11-30 | v4.0 | 3、将跳水和进入游戏检测合并
+# 2023-12-02 | v4.1 | 4、把退出和回车合并；在大厅准备之前加入是否满级判定，并于满级后自动发送邮件通知
+# 2023-12-03 | v4.2 | 5、启用git管理
+# 2023-12-07 | v5.0 | 6、关闭git管理；将代码适配全分辨率、全比例屏幕
+# 2023-12-15 | v5.1 | 7、多进程模式改为多线程模式以降低资源占用
+# 2023-12-22 | v5.2 | 8、由于糖豆人将每周专题获得的经验降为20xp，所以该脚本失去意义了
+# 2024-01-30 | v5.3 | 9、糖豆人恢复每周专题经验值
+# 2024-01-31 | v5.4 | 10、加入季票等级平均执行效率变量，并监控该变量，在异常时发送邮件提醒
+# 2024-02-22 | v5.5 | 11、修复监控脚本执行效率的bug；双倍经验日开始（2024-03-05 20:00结束）
+# 2024-02-22 | v5.6 | 12、删除邮件装饰器和func4
+# 2024-03-05 | v5.7 | 13、暂停func3；适配生存休闲模式；双倍经验日结束
+# 2024-03-13 | v5.8 | 14、适配混合生存赛；更改func1中经验值计算方式
+# 2024-03-22 | v5.9 | 15、适配重力蜂窝迷环试炼
+# 2024-04-18 | v6.0 | 16、双倍经验日开始（2024-04-30 20:00结束）
+# 2024-06-16 | v6.1 | 17、探索模式的新挂季票方案
 # 
 # ======README======
 
@@ -92,16 +93,16 @@ def func1():
     ave_lever = None # 定义平均等级变量，初始化前的值为 None
     start_time = time.time()
     frequency, statement = 0, 0 # 定义进入游戏轮数和卡顿检测变量
-    round_experience = 215 *2 #每局获得经验值，双倍经验日开始（2024-04-30 20:00结束）
+    round_experience = 400 #每局获得经验值
     xp_per_level = 12000 # 升一级所需经验值
 
     # 录入原始x1, y1, rgb1, x2, y2, rgb2
-    x1 = [1771, 997]
-    y1 = [1017, 510]
-    rgb1 = [(28, 28, 28), (253, 112, 88)]
-    x2 = [53]
-    y2 = [1034]
-    rgb2 = [(248, 248, 249)]
+    x1 = [130, 130]
+    y1 = [80, 220]
+    rgb1 = [(49, 49, 49), (49, 49, 49)]
+    x2 = [210, 220]
+    y2 = [33, 169]
+    rgb2 = [(255, 255, 253), (254, 255, 253)]
     # 变化坐标
     x1_offset = calc_x_offset(x1)
     y1_offset = calc_y_offset(y1)
@@ -109,67 +110,37 @@ def func1():
     y2_offset = calc_y_offset(y2)
 
     while True:
-        if is_window_active('FallGuys_client'):
-            if pyautogui.pixelMatchesColor(x1_offset[0], y1_offset[0], rgb1[0], tolerance=2) and pyautogui.pixelMatchesColor(x2_offset[0], y2_offset[0], rgb2[0], tolerance=2):   # 坠落页面计时
-                statement += 1
-                time.sleep(5)
-                if statement >= 18: # 等待n个五秒
-                    pyautogui.press('esc')
-                    statement = 0 # 检测到卡顿，自动退出，同时变量归零
-                    message = '卡住了'
-                    logging.warning(message)    # 打印输出并保存到日志文件  
-                else:
-                    pass
-            elif pyautogui.pixelMatchesColor(x1_offset[1], y1_offset[1], rgb1[1], tolerance=2):
+            if (
+            pyautogui.pixelMatchesColor(x1_offset[0], y1_offset[0], rgb1[0], tolerance=4) and pyautogui.pixelMatchesColor(x2_offset[0], y2_offset[0], rgb2[0], tolerance=4)     # 适用竞速模式
+            or pyautogui.pixelMatchesColor(x1_offset[1], y1_offset[1], rgb1[1], tolerance=4) and pyautogui.pixelMatchesColor(x2_offset[1], y2_offset[1], rgb2[1], tolerance=4)  # 适用生存模式
+            ):
                 statement = 0  # 成功进入游戏，归零变量
                 frequency += 1
                 level_count = frequency * round_experience / xp_per_level
                 total_seconds = time.time() - start_time
                 ave_lever = frequency * round_experience / total_seconds * 3600 / xp_per_level
-                message = "第{:03d}次进入，累计{:0.2f}级，已执行{:0.2f}小时，平均{:0.3f}级/小时".format(frequency, level_count, total_seconds / 3600, ave_lever)
-                # message = "{:03d}：累计{:0.2f}级、{:0.2f}小时、{:0.3f}级/小时".format(frequency, level_count, total_seconds / 3600, ave_lever)
-                logging.info(message)   # 打印输出并保存到日志文件
-                print(message, end='\r')          # 在控制台输出
-                time.sleep(0.4)
-                # pyautogui.press('t') # 视角归正
-                pyautogui.keyDown('s')
-                # pyautogui.keyDown('a')
-                time.sleep(0.6)
-                # pyautogui.keyDown('w')
-                pyautogui.press('space')
-                time.sleep(0.6)
-                # pyautogui.press('space')
-                # pyautogui.keyUp('w')
-                # time.sleep(0.6)
-                # pyautogui.press('space')
-                # time.sleep(7)
-                # pyautogui.keyUp('w')
+                # message = "第{:03d}次进入，累计{:0.2f}级，已执行{:0.2f}小时，平均{:0.3f}级/小时".format(frequency, level_count, total_seconds / 3600, ave_lever)
+                message = "{:03d}：累计{:0.2f}级、{:0.2f}小时、{:0.3f}级/小时".format(frequency, level_count, total_seconds / 3600, ave_lever)
+                logging.info(message)       # 打印输出并保存到日志文件
+                print(message, end='\r')    # 在控制台输出
+                # time.sleep(1)
+                pyautogui.keyDown('r')
+                time.sleep(3)
+                pyautogui.keyUp('r')
                 time.sleep(10)
             else:
                 statement = 0
-                time.sleep(0.4)
-        else:
-            time.sleep(1)
+                time.sleep(1)
 
 # ======退出+回车======
 def func2():
     # 录入原始x1, y1, rgb1, x2, y2, rgb2
-    x1 = [60, 1560, 323, 880, 300, 483, 918, 20, 484, 257, 520, 1000, 40]
-    y1 = [900, 1040, 18, 450, 320, 444, 388, 60, 235, 765, 48, 510, 40]
-    rgb1 = [
-        (49, 49, 49), (49, 49, 49), (34, 34, 38), (50, 201, 235), 
-        (49, 202, 236), (38, 198, 236), (255, 255, 255), (34, 34, 38), 
-        (119, 211, 238), (255, 255, 255), (255, 208, 68), (0, 182, 254), 
-        (254, 106, 0)
-    ]
-    x2 = [1650, 1816, 1450, 1200, 1639, 1300, 1005, 1797, 952, 850, 1550, 1665, 1790]
-    y2 = [1038, 1039, 1000, 125, 400, 550, 600, 1046, 267, 40, 1000, 1035, 1040]
-    rgb2 = [
-        (49, 49, 49), (49, 49, 49), (50, 201, 235), (255, 255, 255), 
-        (255, 255, 255), (42, 176, 209), (188, 0, 130), (49, 49, 49), 
-        (255, 255, 255), (0, 70, 236), (48, 194, 227), (49, 49, 49), 
-        (49, 49, 49)
-    ]
+    x1 = [380]
+    y1 = [920]
+    rgb1 = [(66, 79, 173)]
+    x2 = [1560]
+    y2 = [1000]
+    rgb2 = [(230, 167, 51)]
     # 变化坐标
     x1_offset = calc_x_offset(x1)
     y1_offset = calc_y_offset(y1)
@@ -178,41 +149,19 @@ def func2():
 
     while True:
         if is_window_active('FallGuys_client'):
-            if(
-            pyautogui.pixelMatchesColor(x1_offset[2], y1_offset[2], rgb1[2], tolerance=2) and pyautogui.pixelMatchesColor(x2_offset[2], y2_offset[2], rgb2[2], tolerance=2)       # 启动广告页面
-            or pyautogui.pixelMatchesColor(x1_offset[0], y1_offset[0], rgb1[0], tolerance=2) and pyautogui.pixelMatchesColor(x2_offset[0], y2_offset[0], rgb2[0], tolerance=2)    # 局内观战退出
-            or pyautogui.pixelMatchesColor(x1_offset[1], y1_offset[1], rgb1[1], tolerance=2) and pyautogui.pixelMatchesColor(x2_offset[1], y2_offset[1], rgb2[1], tolerance=2)    # 淘汰结算退出（适配40人和60人两种结算页面）
-            or pyautogui.pixelMatchesColor(x1_offset[3], y1_offset[3], rgb1[3], tolerance=2) and pyautogui.pixelMatchesColor(x2_offset[3], y2_offset[3], rgb2[3], tolerance=2)    # 局内退出菜单页
-            or pyautogui.pixelMatchesColor(x1_offset[4], y1_offset[4], rgb1[4], tolerance=2) and pyautogui.pixelMatchesColor(x2_offset[4], y2_offset[4], rgb2[4], tolerance=2)    # 举报玩家列表
-            or pyautogui.pixelMatchesColor(x1_offset[11], y1_offset[11], rgb1[11], tolerance=2) and pyautogui.pixelMatchesColor(x2_offset[11], y2_offset[11], rgb2[11], tolerance=2) # 出局啦横幅
-                ):
-                pyautogui.keyUp('s') # 结束func1的pyautogui.keyDown('s')
-                # pyautogui.keyUp('a') # 结束func1的pyautogui.keyDown('a')
-                pyautogui.press('esc')
-                time.sleep(0.5)
-            elif(
-            pyautogui.pixelMatchesColor(x1_offset[5], y1_offset[5], rgb1[5], tolerance=2) and pyautogui.pixelMatchesColor(x2_offset[5], y2_offset[5], rgb2[5], tolerance=2)       # 淘汰观战视角退出节目+登录失败+匹配时退出确认
-            or pyautogui.pixelMatchesColor(x1_offset[6], y1_offset[6], rgb1[6], tolerance=2) and pyautogui.pixelMatchesColor(x2_offset[6], y2_offset[6], rgb2[6], tolerance=2)    # 启动页面
-            or pyautogui.pixelMatchesColor(x1_offset[7], y1_offset[7], rgb1[7], tolerance=2) and pyautogui.pixelMatchesColor(x2_offset[7], y2_offset[7], rgb2[7], tolerance=2)    # 经验动画
-            or pyautogui.pixelMatchesColor(x1_offset[8], y1_offset[8], rgb1[8], tolerance=2) and pyautogui.pixelMatchesColor(x2_offset[8], y2_offset[8], rgb2[8], tolerance=2)    # 季票奖励页面
-            or pyautogui.pixelMatchesColor(x1_offset[10], y1_offset[10], rgb1[10], tolerance=2) and pyautogui.pixelMatchesColor(x2_offset[10], y2_offset[10], rgb2[10], tolerance=2)    # 大厅准备
-            or pyautogui.pixelMatchesColor(x1_offset[12], y1_offset[12], rgb1[12], tolerance=2) and pyautogui.pixelMatchesColor(x2_offset[12], y2_offset[12], rgb2[12], tolerance=2)    # 夺冠动画
-                ):
+            if pyautogui.pixelMatchesColor(x1_offset[0], y1_offset[0], rgb1[0], tolerance=2) and pyautogui.pixelMatchesColor(x2_offset[0], y2_offset[0], rgb2[0], tolerance=2): # 检测大厅
+                time.sleep(1)
                 pyautogui.press('enter')
-                time.sleep(0.2)
-            elif pyautogui.pixelMatchesColor(x1_offset[9], y1_offset[9], rgb1[9], tolerance=2) and pyautogui.pixelMatchesColor(x2_offset[9], y2_offset[9], rgb2[9], tolerance=2): # 设置页面
-                time.sleep(5)
-                pyautogui.press('esc')
+                time.sleep(1)
             else:
-                time.sleep(0.5)
+                time.sleep(1)
         else:
             time.sleep(1)
 
 if __name__ == "__main__":
     print(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()), '\n\n============　前言　============\n本脚本仅适用于PC端键鼠用户，请勿在\
 程序执行过程中使用手柄\n当前仅对游戏内语言为简体中文的客户端测试过，其他语言暂未测试\n游戏过程中请将画面设置为全屏，目前暂无针对窗口化模式的适配\n\
-\n============使用说明============\n本脚本包含以下功能：\n1、自动进入游戏并跳水\n2、匹配超过90秒后自动退出匹配\n3、自动跳过夺冠动画、经验动画等\n\
-\n============开始执行============\n请回到游戏，选择每周专题模式，随后程序将自动执行')
+\n============开始执行============\n请回到游戏，选择探索模式，随后程序将自动执行')
     logging.info('开始执行 - 挂季票\n' + '-' * 96) # 保存至日志文件
     p1 = threading.Thread(target = func1)
     p2 = threading.Thread(target = func2)
